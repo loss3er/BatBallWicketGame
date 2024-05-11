595496f3c5faec883289c6541db33798e896f15b
unknown <lossseer@gmail.com>
2024-05-11 23:11:54 +0530

    initial build

---
+++ Script/index.js
@@ -0,0 +1,17 @@
+var c = Math.floor(Math.random() * 3);
+var u = 'bat';
+
+console.log(u);
+
+if (c === 0) {
+  c = 'bat';
+}
+if (c === 1) {
+  c = 'ball';
+} else {
+  c = 'wicket';  
+}
+
+function won(){
+
+}
